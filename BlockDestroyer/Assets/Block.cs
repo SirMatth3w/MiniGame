1ce8650746dff785eed2f76eb91c6340de3ad16d
Kevin <kevingebhardt98@web.de>
2020-07-21 12:12:11 +0200

    basic scripts added and collider

---
+++ BlockDestroyer/Assets/Block.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    private int HP = 3;
+    private void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.name == "ball") {
+            HP--;
+        }
+    }
+    void Update()
+    {
+        if (HP <= 0)
+            Destroy(this.gameObject);
+    }
+}
---
+++ BlockDestroyer/Assets/PlayerPlatform.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPlatform : MonoBehaviour {
+    [SerializeField]
+    private float minPositionX;
+
+    [SerializeField]
+    private float maxPositionX;
+
+    [SerializeField]
+    private KeyCode KeyLeft = KeyCode.LeftArrow;
+
+    [SerializeField]
+    private KeyCode KeyRight = KeyCode.RightArrow;
+
+    [SerializeField]
+    private float Speed = 10;
+
+    private void FixedUpdate() {
+        if (Input.anyKey) {
+            if (Input.GetKey(KeyLeft)|| Input.GetKey(KeyCode.A)) {
+                transform.Translate(-Speed * Time.deltaTime, 0, 0);
+                if (transform.position.x < minPositionX) {
+                    transform.Translate(minPositionX - transform.position.x, 0, 0);
+                }
+            }
+            if (Input.GetKey(KeyRight) || Input.GetKey(KeyCode.D)) {
+                transform.Translate(Speed * Time.deltaTime, 0, 0);
+                if (transform.position.x > maxPositionX) {
+                    transform.Translate(maxPositionX - transform.position.x, 0, 0);
+                }
+            }
+        }
+    }
+}
